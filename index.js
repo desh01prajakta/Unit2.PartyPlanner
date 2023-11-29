66ad35f6a1372bb72706a4f93ac7dea8cf728b5b
desh01prajakta <prajudeshmukh11@gmail.com>
2023-11-29 02:51:24 -0600

    adds value

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const name = document.getElementById ("partyName").value;
-const date = document.getElementById ("date").value;
-const time = document.getElementById ("time"). value;
-const description = document.getElementById ("description").value;
-const location = document.getElementById ("location").value;
-const partyForm = document.getElementById ("partyForm").value;
-const newPartyForm = document.getElementById ("newPartyForm").value;
-const partyList = document.getElementById ("partList");
-const listItem = document.createElement ("li");
+/*const name = document.getElementById ("partyName")
+const date = document.getElementById ("date")
+const time = document.getElementById ("time")
+const description = document.getElementById ("description")
+const location = document.getElementById ("location")*/
+const partyForm = document.getElementById ("partyForm")
+const newPartyForm = document.getElementById ("newPartyForm")
+const partyList = document.getElementById ("partyList");
+/*const listItem = document.createElement ("li");
 listItem.className = "party";
 
 listItem.innerHTML = 
@@ -17,8 +17,58 @@ async function fetchParties(){
     try{
     const response = await fetch('https://fsa-crud-2aa9294fe819.herokuapp.com/api/2109-CPU-RM-WEB-PT/event')
 const data = await response.json();
+party
 renderPartyList()
 }catch (error){
     console.error('error fetching parties:', error.message)
 }
+}*/
+document.addEventListener('DOMContentLoaded', function () {
+    const partyList = document.getElementById('party-list');
+    const partyForm = document.getElementById('new-party-form');
+
+const parties =[
+    {name:"eva", date:"2023-12-01", time: "18:00", location: "venue1", description:"lorem ipsum"},
+    {name: "john", date: "2023-12-06", time: "19:00", location: "venue2", description: "lorem ipsum1"}
+]
+console.log(parties)
+
+function renderPartyList(){
+    partyList.innerHTML = '';
+    parties.forEach((party, index) => {
+        const partyItem = document.createElement('div')
+        partyItem.className = 'party-item';
+        partyItem.innerHTML = `<strong>${party.name}</strong><br>
+        date: ${party.date}<br>
+        time: ${party.time}<br>
+        location: ${party.location}<br>
+        description: ${party.description}<br>
+        <button oneclick = "deleteParty(${index})">Delete</button>`;
+        partyList.appendChild(partyItem)
+    })
 }
+
+window.deleteparty = function(index){
+    parties.splice(index,1);
+    renderPartyList();
+}
+
+partyForm.addEventListener('submit', function (event) {
+event.preventDefault();
+const newName = document.getElementById('partName').value;
+const newDate = document.getElementById('date').value;
+const newLocation = document.getElementById('location').value;
+const newTime = document.getElementById('time').value;
+const newDescription = document.getElementById('description').value;
+parties.push({
+    name: newName,
+    date: newDate,
+    time: newTime,
+    location: newLocation,
+    description: newDescription
+})
+renderPartyList();
+partyForm.reset();
+})
+renderPartyList();
+});
