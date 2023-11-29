4dcf40b928713ad3a0b78419850f98e3508f7019
desh01prajakta <prajudeshmukh11@gmail.com>
2023-11-28 23:59:39 -0600

    adds value

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const date = document.getElementById ("date").value;
 const time = document.getElementById ("time"). value;
 const description = document.getElementById ("description").value;
 const location = document.getElementById ("location").value;
+const partyForm = document.getElementById ("partyForm").value;
+const newPartyForm = document.getElementById ("newPartyForm").value;
 const partyList = document.getElementById ("partList");
 const listItem = document.createElement ("li");
 listItem.className = "party";
@@ -10,3 +12,13 @@ listItem.className = "party";
 listItem.innerHTML = 
 <button class = "deleteButton" oneclick = "deleteParty(this)">Delete</button>;
 partyList.appendChild(listItem);
+
+async function fetchParties(){
+    try{
+    const response = await fetch('https://fsa-crud-2aa9294fe819.herokuapp.com/api/2109-CPU-RM-WEB-PT/event')
+const data = await response.json();
+renderPartyList()
+}catch (error){
+    console.error('error fetching parties:', error.message)
+}
+}
